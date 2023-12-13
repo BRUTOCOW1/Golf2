656690da219175e6cff9b34b9b2602c9f38b22fc
Ben Brutocao <bdb47@duke.edu>
2023-12-12 19:39:32 -0600

    last one

---
+++ lib/Conditions.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <iostream>
+#include <ostream>
+
+
+enum Temperature {COLD, COOL, TEPID, WARM, HOT};
+enum Moisture {MUDDY, DAMP, DRY, ARID};
+
+class Conditions {
+
+    
+
+    public:
+
+    Temperature temp;
+    Moisture hydrometer;
+
+    protected:
+
+        
+
+
+        Direction direction;
+        Type type;
+        int distance;
+
+};
+
